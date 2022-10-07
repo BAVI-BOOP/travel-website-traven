59379b1d9e6029ab9a5dc5d815ab76a88a31501a
Ahmet <birceste@gmail.com>
2022-10-07 03:19:43 +0300

    api finished, language support added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+import translate from "./translate.js";
+
 const hotelOptions = {
   method: "GET",
   headers: {
@@ -10,17 +12,47 @@ const flightOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "3be46c2a52msh1c8fa446280fb71p1a6d3fjsn365b3c0e6b6b",
-    "X-RapidAPI-Host": "skyscanner50.p.rapidapi.com",
+    "X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com",
   },
 };
+
+let hotel = document.getElementById("hotel");
+let flight = document.getElementById("flight");
+let navTabs = document.getElementById("nav-tabs");
+let hotelOrFlight = "hotel";
+
+let checkoutContainer = document.getElementById("checkoutContainer");
+let destinationContainer = document.getElementById("destinationContainer");
+
+navTabs.addEventListener("click", (event) => {
+  if (event.target.id === "hotel") {
+    hotel.style.color = "#ff666f";
+    flight.style.color = "black";
+    hotelOrFlight = "hotel";
+    checkoutContainer.style.display = "inline-block";
+    checkoutContainer.style.display = "flex";
+    destinationContainer.style.display = "none";
+  } else if (event.target.id === "flight") {
+    hotel.style.color = "black";
+    flight.style.color = "#ff666f";
+    hotelOrFlight = "flight";
+    destinationContainer.style.display = "inline-block";
+    destinationContainer.style.display = "flex";
+    checkoutContainer.style.display = "none";
+  }
+
+  console.log(hotelOrFlight);
+});
 
 const search = document.querySelector(".search");
 const results = document.querySelector(".search-results");
 
-const arrival = document.getElementById("location");
 const checkIn = document.getElementById("checkIn");
 const checkOut = document.getElementById("checkOut");
+
 const passengers = document.getElementById("passengers");
+
+const arrival = document.getElementById("location");
 
 // Hotel datas that will be displayed
 let photo = document.querySelector(".photo");
@@ -37,50 +69,115 @@ function showResult(event) {
   let checkOutInfo = checkOut.value;
   let passengersInfo = passengers.value;
 
-  fetch(
-    `https://hotels4.p.rapidapi.com/locations/v2/search?query=${arrivalInfo}&locale=en_US&currency=USD`,
-    hotelOptions
-  )
-    .then((response) => response.json())
-    .then((response) =>
-      fetch(
-        `https://hotels4.p.rapidapi.com/properties/list?destinationId=${response.suggestions[0].entities[0].destinationId}&pageNumber=1&pageSize=5&checkIn=${checkInInfo}&checkOut=${checkOutInfo}&adults1=${passengersInfo}&sortOrder=PRICE&locale=en_US&currency=USD`,
-        hotelOptions
-      )
-        .then((hotelData) => hotelData.json())
-        .then((responseTwo) => {
-          console.log(responseTwo);
-          results.innerHTML = "";
-          responseTwo.data.body.searchResults.results.forEach((object) => {
-            results.innerHTML += `        
-            <div class="card">
-            <div class="card-top">
-              <p class="hotelName">${object.name}</p>
-            </div>
-            <div class="card-info">
-              <div>Photo</div>
-              <div>Adress</div>
-              <div>Rating</div>
-              <div>Price</div>
-            </div>
-            <div class="card-bottom">
-              <div>
-                <img
-                  src="${object.optimizedThumbUrls.srpDesktop}"
-                  alt=""
-                  class="photo"
-                />
-              </div>
-              <div><p class="address">${object.address.streetAddress}</p></div>
-              <div class="rating">${object.guestReviews.rating}</div>
-              <div class="price">${object.ratePlan.price.current}</div>
-            </div>
-          </div>`;
-          });
-        })
-        .catch((err) => console.error(err))
+  if (hotelOrFlight === "hotel") {
+    fetch(
+      `https://hotels4.p.rapidapi.com/locations/v2/search?query=${arrivalInfo}&locale=en_US&currency=USD`,
+      hotelOptions
     )
-    .catch((err) => console.error(err));
+      .then((response) => response.json())
+      .then((response) =>
+        fetch(
+          `https://hotels4.p.rapidapi.com/properties/list?destinationId=${response.suggestions[0].entities[0].destinationId}&pageNumber=1&pageSize=5&checkIn=${checkInInfo}&checkOut=${checkOutInfo}&adults1=${passengersInfo}&sortOrder=PRICE&locale=en_US&currency=USD`,
+          hotelOptions
+        )
+          .then((hotelData) => hotelData.json())
+          .then((responseTwo) => {
+            console.log(responseTwo);
+            results.innerHTML = "";
+            responseTwo.data.body.searchResults.results.forEach((object) => {
+              results.innerHTML += `        
+              <div class="card">
+              <div class="card-top">
+                <p class="hotelName">${object.name}</p>
+              </div>
+              <div class="card-info">
+                <div>Photo</div>
+                <div>Adress</div>
+                <div>Rating</div>
+                <div>Price</div>
+              </div>
+              <div class="card-bottom">
+                <div>
+                  <img
+                    src="${object.optimizedThumbUrls.srpDesktop}"
+                    alt=""
+                    class="photo"
+                  />
+                </div>
+                <div><p class="address">${object.address.streetAddress}</p></div>
+                <div class="rating">${object.guestReviews.rating}</div>
+                <div class="price">${object.ratePlan.price.current}</div>
+              </div>
+            </div>`;
+            });
+          })
+          .catch((err) => console.error(err))
+      )
+      .catch((err) => console.error(err));
+  } else if (hotelOrFlight === "flight") {
+    destinationLocation = document.getElementById("destinationInfo");
+    destinationInfo = destinationLocation.value;
+    console.log(destinationInfo);
+
+    fetch(
+      `https://priceline-com-provider.p.rapidapi.com/v1/flights/locations?name=${arrivalInfo}`,
+      flightOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        let fromIata = response[0].id;
+        fetch(
+          `https://priceline-com-provider.p.rapidapi.com/v1/flights/locations?name=${destinationInfo}`,
+          flightOptions
+        )
+          .then((responseTwo) => responseTwo.json())
+          .then((responseTwo) => {
+            let toIata = responseTwo[0].id;
+            fetch(
+              `https://priceline-com-provider.p.rapidapi.com/v1/flights/search?itinerary_type=ONE_WAY&class_type=ECO&location_arrival=${toIata}&date_departure=${checkInInfo}&location_departure=${fromIata}&sort_order=PRICE&price_max=20000&number_of_passengers=${passengersInfo}&price_min=100`,
+              flightOptions
+            )
+              .then((response) => response.json())
+              .then((responseThree) => {
+                let code = responseThree.totalTripSummary.airline[0].code;
+                function isobj(company) {
+                  return company.code === code;
+                }
+                let airlineObject = responseThree.airline.find(isobj);
+                let airlineName = airlineObject.name;
+                let airlineUrl = airlineObject.websiteUrl;
+                console.log(code);
+                console.log(responseThree);
+                results.innerHTML = "";
+                results.innerHTML = `
+                <div class="card">
+                <div class="card-top">
+                  <p class="flightName">${airlineName}</p>
+                </div>
+                <div class="card-info">
+                  <div lng-tag="Airline Website">Airline Website</div>
+                  <div lng-tag="Departure">Departure</div>
+                  <div lng-tag="Arrival">Arrival</div>
+                  <div lng-tag="Cheapest Price">Cheapest Price</div>
+                </div>
+                <div class="card-bottom">
+                  <div>
+                  <a href=https://${airlineUrl} target="_blank">Go to Airline website</a>
+                  </div>
+                  <div><p class="address">From:${arrivalInfo}  </p></div>
+                  <div class="to">To: ${destinationInfo}</div>
+                  <div class="price-flight">${responseThree.totalTripSummary.airline[0].lowestTotalFare.amount}$</div>
+                </div>
+              </div>               
+                `;
+              })
+              .catch((err) => console.error(err));
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
+  }
 }
 
 search.addEventListener("click", showResult);
+
---
+++ assets/js/ind.js
@@ -0,0 +1,71 @@
+import Translate from "./translate.js";
+
+//This function will be called when user click changing language
+function translate(lng, tagAttr) {
+  var translate = new Translate();
+  translate.init(tagAttr, lng);
+  translate.process();
+  if (lng == "en") {
+    $("#enTranslator").css("color", "#f4623a");
+    $("#khTranslator").css("color", "#212529");
+  }
+  if (lng == "kh") {
+    $("#khTranslator").css("color", "#f4623a");
+    $("#enTranslator").css("color", "#212529");
+  }
+}
+$(document).ready(function () {
+  //This is id of HTML element (English) with attribute lng-tag
+  $("#enTranslator").click(function () {
+    translate("en", "lng-tag");
+  });
+  //This is id of HTML element (Khmer) with attribute lng-tag
+  $("#esTranslator").click(function () {
+    translate("es", "lng-tag");
+  });
+  //This is id of HTML element (Khmer) with attribute lng-tag
+  $("#trTranslator").click(function () {
+    translate("tr", "lng-tag");
+  });
+  //This is id of HTML element (Khmer) with attribute lng-tag
+  $("#deTranslator").click(function () {
+    translate("de", "lng-tag");
+  });
+});
+
+
+// let deTranslate = document.getElementById("deTranslate");
+// let esTranslate = document.getElementById("esTranslate");
+// let trTranslate = document.getElementById("trTranslate");
+
+// esTranslate.addEventListener("load", translate("es", "lng-tag"));
+// trTranslate.addEventListener("load", translate("tr", "lng-tag"));
+// deTranslate.addEventListener("load", translate("de", "lng-tag"));
+// enTranslate.addEventListener("load", translate("en", "lng-tag"));
+
+
+let languages = document.querySelector("#languages");
+
+languages.addEventListener("change", (event) => {
+  console.log(event.target.value)
+  if (event.target.value === "de") {
+    translate("de", "lng-tag");
+  }
+  if (event.target.value === "es") {
+    translate("es", "lng-tag");
+    console.log('askdjal')
+  }
+  if (event.target.value === "en") {
+    translate("en", "lng-tag");
+    console.log("anna");
+  }
+  if (event.target.value === "tr") {
+    translate("tr", "lng-tag");
+    console.log("aaan");
+  }
+});
+
+
+let body = document.querySelector("body");
+
+body.addEventListener("load", translate("de", "lng-tag"));
